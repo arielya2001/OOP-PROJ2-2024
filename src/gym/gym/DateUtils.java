c74683cfc4c266a314867e153d9de287746a2da8
amitd55 <156925877+amitd55@users.noreply.github.com>
2024-12-08 12:28:43 +0200

    Add files via upload

--- src/gym/gym/DateUtils.java
+++ src/gym/gym/DateUtils.java
@@ -6,28 +6,29 @@ import java.time.format.DateTimeFormatter;
 
 public class DateUtils {
 
-    private static final DateTimeFormatter FORMATTER=DateTimeFormatter.ofPattern("dd-mm-yyyy");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     private static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
 
 
-    public static LocalDate parseDate(String date) throws DateTimeException{
-        return LocalDate.parse(date,FORMATTER);
+    public static LocalDate parseDate(String date) throws DateTimeException {
+        if (date.length()>10)
+        {
+            date=date.substring(0,10);
+        }
+        return LocalDate.parse(date, FORMATTER);
     }
 
-    public static String formatDate(LocalDate date)
-    {
+    public static String formatDate(LocalDate date) {
         return date.format(FORMATTER);
     }
 
-    public static boolean isOverAge(String birthDate, int age)
-    {
-        LocalDate date=parseDate(birthDate);
+
+    public static boolean isOverAge(String birthDate, int age) {
+        LocalDate date = parseDate(birthDate);
         return date.plusYears(age).isBefore(LocalDate.now()) || date.plusYears(age).isEqual(LocalDate.now());
     }
-    public static boolean dateNotPassed(String date)
-    {
+
+    public static boolean dateNotPassed(String date) {
         return parseDate(date).isAfter(LocalDate.now());
     }
-
-
 }
